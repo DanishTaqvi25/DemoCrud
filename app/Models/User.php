29ba1cb4d75ec72a751ea2b637ac305e9e34b3db
nigar-igen <142220594+nigar-igen@users.noreply.github.com>
2023-11-22 00:36:13 +0530

    user changes

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Contracts\Auth\Authenticatable;
 
 class User extends Model
-{
-     
+{  
     protected $fillable = ['name', 'email', 'password'];
 
     public function setPasswordAttribute($value)
