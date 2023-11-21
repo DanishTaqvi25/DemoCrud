e8ab941dcc6787c59409bda62c108c525221c5ae
nigar-igen <142220594+nigar-igen@users.noreply.github.com>
2023-11-21 20:39:46 +0530

    myProject

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function showRegistrationForm()
+    {
+        return view('register');
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|alpha',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('register')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        User::create($request->all());
+
+        return redirect('login');
+    }
+
+    public function showLoginForm()
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard');
+        }
+
+        return redirect('login')->withErrors(['login' => 'Invalid email or password']);
+    }
+
+    public function dashboard()
+    {
+        return view('dashboard')->with('user', Auth::user());
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('login');
+    }
+}
+
