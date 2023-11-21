59e6d2f2805f22c50dca3d590ca77f2dcd718564
nigar-igen <142220594+nigar-igen@users.noreply.github.com>
2023-11-21 20:41:31 +0530

    MyProject

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController; 
+use App\Http\Controllers\ModuleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Auth\LoginController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+ 
+Route::get('/', [AuthController::class, 'showRegistrationForm']);
+Route::get('/register', [AuthController::class, 'showRegistrationForm']);
+Route::post('/register-user', [AuthController::class, 'register']);
+Route::get('/login', [AuthController::class, 'showLoginForm']);
+Route::post('/login-user', [AuthController::class, 'login']);
+// Route::get('/register', 'AuthController@showRegistrationForm');
+// Route::post('/register', 'AuthController@register');
+// Route::get('/login', 'AuthController@showLoginForm');
+// Route::post('/login', 'AuthController@login');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', 'AuthController@dashboard');
+    Route::get('/logout', 'AuthController@logout');
 });
